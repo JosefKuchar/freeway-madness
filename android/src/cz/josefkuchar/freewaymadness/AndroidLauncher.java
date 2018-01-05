4310437256dbdc42321d3cfb94bd343ec74a74ea
Josef Kuchař <josef.kuchar267@gmail.com>
2018-01-05 23:13:23 +0100

    Init

---
+++ android/src/cz/josefkuchar/freewaymadness/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package cz.josefkuchar.freewaymadness;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import cz.josefkuchar.freewaymadness.FreewayMadness;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useGyroscope = false;
+		initialize(new FreewayMadness(), config);
+	}
+}
